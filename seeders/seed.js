398ecb8fe77fcc7c1b65d45ee616ddb744190805
Mark Wilson <69028877+markwilson107@users.noreply.github.com>
2021-01-07 15:11:17 +0800

    Update seed.js

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let Workout = require("../models/workoutModel");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
